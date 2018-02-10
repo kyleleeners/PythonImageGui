424191963dd6f50d8d2815da5e47188b0bcc03ff
KyleLeeners <kyle.jleeners@gmail.com>
2018-02-09 22:31:17 -0800

    added scraper, colour extractor, and some pre-processed training data

--- Gui.py
+++ Gui.py
@@ -1,38 +1,44 @@
 from tkinter import *
 from tkinter import filedialog, messagebox
-import PIL.Image
-import PIL.ImageTk
 
 
 class Window(Frame):
 
-    def __init__(self, root = None):
-        Frame.__init__(self, root)
-        self.root = root
-        self.init_window()
-        self.main_label = Label(self.root)
-        Button(self.root, text="upload", fg="red", command=self.upload_image).pack(side=BOTTOM)
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
 
-    def init_window(self):
-        self.root.title("GUI")
-        self.pack(fill=BOTH, expand=1)
+        self.master = master
+        self.init_window()
+        self.main_frame = self.master
+        self.main_label = Label(self.main_frame)
+
+        Button(self.master, text="upload", fg="red", command=self.upload_image).pack(side=BOTTOM)
+
+        self.rgb_value = Label(self.master)
+        self.main_label.bind("<Button-1>", self.on_click)
 
     def upload_image(self):
         filename = filedialog.askopenfilename(initialdir="/", title="Select file")
         try:
-            load = PIL.Image.open(filename)
-            render = PIL.ImageTk.PhotoImage(load)
-            self.main_label.image = render
-            self.main_label.config(image=render)
+            img = PhotoImage(file=filename)
+            self.main_label.image = img
+            self.main_label.config(image=img)
             self.main_label.pack()
         except Exception:
             messagebox.showerror("Error", "Not a valid image file, try with .png")
 
     def on_click(self, event):
-        print("Clicked at: ", event.x, event.y)
+        image = self.main_label.image
+        r, g, b = image.get(event.x, event.y)
+        self.rgb_value.config(text="r: %.3f, g: %.3f, b: %.3f" % (r, g, b))
+        self.rgb_value.pack()
+
+    def init_window(self):
+        self.master.title("GUI")
+        self.pack(fill=BOTH, expand=1)
 
 
 if __name__ == "__main__":
-    master = Tk()
-    app = Window(master)
-    master.mainloop()
+    root = Tk()
+    app = Window(root)
+    root.mainloop()
