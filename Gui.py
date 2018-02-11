05a156f2777817c0f5253f38a550df366a30a54c
KyleLeeners <kyle.jleeners@gmail.com>
2018-02-11 01:06:41 -0800

    now you can train the model with the GUI

--- Gui.py
+++ Gui.py
@@ -3,22 +3,43 @@ from tkinter import filedialog, messagebox
 from model import Model
 import numpy as np
 
+
 class Window(Frame):
 
     def __init__(self, ml_model, master=None):
         Frame.__init__(self, master)
-
         self.model = ml_model
         self.master = master
-        self.init_window()
+        self.initialize()
+
+
+    def initialize(self):
+        self.master.title("GUI")
+        self.pack(fill=BOTH, expand=1)
+        self.cur_rgb = (-1, -1, -1)
         self.main_frame = Frame(self.master).pack()
         self.bottom_frame = Frame(self.master).pack(side=BOTTOM)
         self.main_label = Label(self.main_frame)
+        self.prediction = Label(self.bottom_frame)
 
         Button(self.main_frame, text="upload", fg="red", command=self.upload_image).pack()
-
-        self.prediction = Label(self.bottom_frame)
         self.main_label.bind("<Button-1>", self.on_click)
+
+        def select_option():
+            if self.cur_rgb == (-1, -1, -1):
+                return
+            try:
+                model.update(var.get(), self.cur_rgb)
+            except Exception as error:
+                messagebox.showerror("Error", str(error))
+
+
+        var = StringVar(self.bottom_frame)
+        choices = ["Red", "Green", "Yellow", "Blue", "Orange", "Purple", "Pink", "White"]
+        OptionMenu(self.bottom_frame, var, *choices).pack(side=BOTTOM)
+        Button(root, text="Wrong Colour? Click to train!", fg="green", command=select_option).pack(side=BOTTOM)
+
+
 
     def upload_image(self):
         filename = filedialog.askopenfilename(initialdir="/", title="Select file")
@@ -33,15 +54,11 @@ class Window(Frame):
     def on_click(self, event):
         image = self.main_label.image
         r, g, b = image.get(event.x, event.y)
-
+        self.cur_rgb = (r, g, b)
         prediction = self.model.predict(np.array([[r, g, b]]))
         self.prediction.config(text=prediction[0])
         self.prediction.pack()
 
-
-    def init_window(self):
-        self.master.title("GUI")
-        self.pack(fill=BOTH, expand=1)
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -19,19 +19,43 @@ class Model:
                 concatenated_data = data
             else:
                 concatenated_data = np.vstack((concatenated_data, data))
+        self.remove_datasets(concatenated_data)
         return concatenated_data
 
+    def remove_datasets(self, superset):
+        for set in os.listdir("Data/"):
+            file_path = os.path.join("Data/", set)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(e)
+        self.create_superset(superset)
+
+    def create_superset(self, superset):
+        with open('Data/all_data.pickle', 'wb') as handle:
+            pickle.dump(superset, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
     def fit(self):
-        data = self.load_datasets()
-        X,y = data[:,:3], data[:,3:4].ravel()
+        X,y = self.data[:,:3], self.data[:,3:4].ravel()
         self.model = KNeighborsClassifier(5)
         self.model.fit(X,y)
 
     def predict(self, item_to_classify):
         return self.model.predict(item_to_classify)
 
+    def update(self, colour_name, rgb_value):
+        if colour_name == "":
+            raise Exception("No colour selected")
+        new_entry = [[rgb_value[0], rgb_value[1], rgb_value[2], colour_name]]
+        self.data = np.vstack((self.data, new_entry))
+        self.remove_datasets(self.data)
+        self.fit()
+
     def __init__(self):
         self.model = None
+        self.data = self.load_datasets()
+
 
 
 model = Model()
